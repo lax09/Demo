e0a57808f9f47ec02a1a7ee22d5977560823c457
lax09 <lakshmikataru489@gmail.com>
2019-06-08 00:12:45 +0530

    first commit

---
+++ src/test/java/com/qa/tests/AppTest.java
@@ -0,0 +1,21 @@
+package com.qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void sum(){
+    	System.out.println("sum method");
+    	
+    	int a=10;
+    	int b=20;
+    	Assert.assertEquals(30, a+b);
+    }
+   
+}
