a53609d88613b78344db904d8c16f5c161637113
lax09 <lakshmikataru489@gmail.com>
2019-06-09 13:35:36 +0530

    nnn

--- src/test/java/com/qa/tests/Demo5.java
+++ src/test/java/com/qa/tests/Demo5.java
@@ -2,25 +2,17 @@ package com.qa.tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-//my first comments
+
 
 /**
  * Unit test for simple App.
  */
-public class Demo2 
+public class Demo5 
 {
     @Test
     public void sum(){
     	System.out.println("sum method");
-    	
-    	int a=10;
-    	int b=20;
-    	Assert.assertEquals(30, a+b);
-    }
-    
-    @Test
-    public void sum1(){
-    	System.out.println("sum method");
+    	System.out.println("new change");
     	
     	int a=10;
     	int b=20;
